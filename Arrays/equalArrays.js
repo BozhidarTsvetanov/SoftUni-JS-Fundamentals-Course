ecfa959dee56b33712d99401313f076de12292a3
BozhidarTsvetanov <bcvetanov23@gmail.com>
2026-09-07 23:21:04 +0300

    Create equalArrays.js

---
+++ Arrays/equalArrays.js
@@ -0,0 +1,18 @@
+function equalArrays(input1, input2) {
+    
+for (let index = 0; index < input1.length; index++) {
+   let num1 = Number(input1[index])
+    
+}
+for (let index = 0; index < input2.length; index++) {
+    let num2 = Number(input2[index])
+    
+}
+let same = true
+if (input1[index] !== input2[index]) {
+    
+}
+}
+equalArrays(['10','20','30'],
+
+['10','20','30'])
