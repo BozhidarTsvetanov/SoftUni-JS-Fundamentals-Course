aeca6fbc7350cf9dd934f40f17d207e7943b66ff
BozhidarTsvetanov <bcvetanov23@gmail.com>
2026-09-07 23:10:47 +0300

    Create dayOfWeek.js

---
+++ Arrays/dayOfWeek.js
@@ -0,0 +1,13 @@
+function dayOfWeek(day) {
+    
+if (day <= 7) {
+    arr = ['Invalid day!', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    console.log(arr[day]);
+    
+}else{
+    console.log('Invalid day!');
+    
+}
+
+}
+dayOfWeek(0)
