90ee9d8b6a1dbe2ef6ca2e08f62110178071da99
BozhidarTsvetanov <bcvetanov23@gmail.com>
2026-09-07 23:07:56 +0300

    Create sumFirstAndLastArrayElements.js

---
+++ Arrays/sumFirstAndLastArrayElements.js
@@ -0,0 +1,9 @@
+function sumFirstAndLastArrayElements(input) {
+    
+let firntNum = input[0]
+let lastNum = input[input.length-1]
+let sum = firntNum + lastNum
+console.log(sum);
+
+}
+sumFirstAndLastArrayElements([11, 58, 69])
