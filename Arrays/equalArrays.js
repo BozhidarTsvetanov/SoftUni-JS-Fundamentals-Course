b625b712fa4b9bb00f32aff1337896b291883970
BozhidarTsvetanov <bcvetanov23@gmail.com>
2026-09-07 23:26:50 +0300

    Update equalArrays.js

--- Arrays/equalArrays.js
+++ Arrays/equalArrays.js
@@ -1,18 +1,22 @@
 function equalArrays(input1, input2) {
-    
-for (let index = 0; index < input1.length; index++) {
-   let num1 = Number(input1[index])
-    
-}
-for (let index = 0; index < input2.length; index++) {
-    let num2 = Number(input2[index])
-    
-}
-let same = true
-if (input1[index] !== input2[index]) {
-    
-}
-}
-equalArrays(['10','20','30'],
 
-['10','20','30'])
+    let sum = 0;
+
+    for (let index = 0; index < input1.length; index++) {
+
+        if (input1[index] !== input2[index]) {
+            console.log(`Arrays are not identical. Found difference at ${index} index.`);
+            return;
+        }
+
+        sum += Number(input1[index]);
+    }
+
+    console.log(`Arrays are identical. Sum: ${sum}`);
+}
+
+equalArrays(
+    ['10', '20', '30'],
+    ['10', '20', '30']
+);
+
