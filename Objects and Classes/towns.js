31579feaacfb969d5968d1093f728853ad47ab04
BozhidarTsvetanov <bcvetanov23@gmail.com>
2026-09-08 14:47:29 +0300

    Create towns.js

---
+++ Objects and Classes/towns.js
@@ -0,0 +1,19 @@
+function towns(input) {
+
+    for (let element of input) {
+        let [town, latitude, longitude] = element.split(' | ')
+        latitude = Number(latitude).toFixed(2)
+        longitude = Number(longitude).toFixed(2)
+
+        let obj = {
+            town: town,
+            latitude: latitude,
+            longitude: longitude
+        }
+        console.log(obj);
+        
+    }
+}
+towns(['Sofia | 42.696552 | 23.32601',
+'Beijing | 39.913818 | 116.363625']
+)
