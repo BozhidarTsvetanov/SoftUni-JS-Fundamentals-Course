2f13d426c4eae1d3fe8a567ece78d2949d96216d
BozhidarTsvetanov <bcvetanov23@gmail.com>
2026-09-08 14:34:56 +0300

    Create lowerOrUpper.js

---
+++ Data Types and Variables/lowerOrUpper.js
@@ -0,0 +1,12 @@
+function lowerOrUpper(input) {
+    
+if (input === input.toUpperCase()) {
+    console.log('upper-case');
+    
+}else{
+    console.log('lower-case');
+    
+}
+
+}
+lowerOrUpper('L')
